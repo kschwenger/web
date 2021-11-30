270163cb45a22e9a709b2ed993ac24309b480f38
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-30 18:39:06 +0000

    qewdfqw

--- web.py
+++ web.py
@@ -27,10 +27,12 @@ else:
 print('<br>')
 print('Place Ships <br>')
 print('<form action="/cgi-bin/web.py" method="POST">')
-if Coordinates.count('A1') > 0:
-  print('<input type="checkbox" name="option" value="A1" checked>')
-else:
-  print('<input type="checkbox" name="option" value="A1">')
+
+if isinstance(Coordinates, list):
+  if Coordinates.count('A1') > 0:
+    print('<input type="checkbox" name="option" value="A1" checked>')
+  else:
+    print('<input type="checkbox" name="option" value="A1">')
 
 
 print('<input type="checkbox" name="option" value="A2">')
