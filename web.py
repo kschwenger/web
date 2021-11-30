cc41010850c4af0a2e68121085b8fb99e75e89da
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-30 19:15:04 +0000

    qf

--- web.py
+++ web.py
@@ -21,7 +21,9 @@ def checking(coord):
       print('<input type="checkbox" name="option" value=coord>')
   else:
     print('<input type="checkbox" name="option" value=coord>')
-  if count >= 8: print('<br>')
+  if count >= 8: 
+    print('<br>')
+    count = 0
 
 
 # html code
@@ -49,8 +51,8 @@ print('<form action="/cgi-bin/web.py" method="POST">')
 #else:
   #print('<input type="checkbox" name="option" value="A1">')
 
-list = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8']
-for elem in list:
+AllCoords = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8']
+for elem in AllCoords:
   checking(elem)
 
 """
