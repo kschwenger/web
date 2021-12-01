d7d9efc46b5a5cd0f203c1ef37ebdc7d992b93b7
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-12-01 18:19:12 +0000

    asdfaf

--- reading.py
+++ reading.py
@@ -2,7 +2,13 @@ import json
 from time import sleep
 
 submissions = 0
-ships = {}
+#ships = {}
+
+def separate(l, k):
+  for i in range(len(k)):
+    if k[i] in l:
+      r = k[i]
+      l.remove[r]
 
 while True:         #try a counter variable, stops after 4 ships saved? saves each submitted to a different integer key?
   with open('web.txt', 'r') as f:
@@ -12,16 +18,20 @@ while True:         #try a counter variable, stops after 4 ships saved? saves ea
     submissions += 1
 
     if submissions == 1:
-      ships["Battleship"] = data["Coordinates"]
+      #ships["Battleship"] = data["Coordinates"]
+      Battleship = data["Coordinates"]
 
     elif submissions == 2:
-      ships["Submarine 1"] = data["Coordinates"]
-
+      #ships["Submarine 1"] = data["Coordinates"]
+      Submarine1 = data["Coordinates"]
     elif submissions == 3:
-      ships["Submarine 2"] = data["Coordinates"]
+      #ships["Submarine 2"] = data["Coordinates"]
+      Submarine2 = data["Coordinates"]
 
     elif submissions == 4:
-      ships["Destroyer"] = data["Coordinates"]
+      #ships["Destroyer"] = data["Coordinates"]
+      Destroyer = data["Coordinates"]
+
 
     #ships["ship"] = data["Coordinates"]
   #else: 
@@ -37,7 +47,15 @@ while True:         #try a counter variable, stops after 4 ships saved? saves ea
   if submissions > 3:
     break
 
+separate(Battleship, Submarine1)
+separate(Submarine1, Submarine2)
+separate(Submarine2, Destroyer)
+
 print("All ships selected")
+print(Battleship)
+print(Submarine1)
+print(Submarine2)
+print(Destroyer)
 # if len = 4: save [0-3] as battleship
 # if len = 7: save new coords as submarine
 # if len = 10: save new coords as submarine
