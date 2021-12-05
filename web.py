171ce6fe299664da46c3eb07a5695c2836603937
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-12-05 23:38:46 +0000

    center?

--- web.py
+++ web.py
@@ -8,10 +8,11 @@ from urllib.request import urlopen # use to send/receive data
 from urllib.parse import urlencode # use to structure a GET string
 
 # global variables 
+# for checkboxes
 AllCoords = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8']
-count = 0 # for checking func
+count = 0 # for checking function
 Rowdict = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8} # for sorting and validating purposes
-Completed = False
+Completed = False # True once all ships placed
 
 def checking(coord): # check each coord to see if already picked, display as checked if so
   global count
