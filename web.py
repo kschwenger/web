863fa02e6b3498e94e6ed112cb8071f326d1a532
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-12-11 22:43:17 +0000

    thh

--- web.py
+++ web.py
@@ -117,7 +117,7 @@ dataFromhtml = cgi.FieldStorage()
 Coordinates = dataFromhtml.getvalue('option') # save chosen coordinates as a list
 Submit = dataFromhtml.getvalue('submitted')
 
-print('<div style="float: left;">')
+print('<div style="float:left; margin:10px; margin-left:50px;">')
 
 print('<h3>')
 
