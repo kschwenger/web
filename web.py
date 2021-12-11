2c3313507031106ddc7fb4b2e8699721accd994c
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-12-11 22:57:07 +0000

    dasfdsfag

--- web.py
+++ web.py
@@ -103,7 +103,7 @@ Submit = dataFromhtml.getvalue('submitted')
 
 print('<div style="float:left; background:gray; margin:10px; margin-left:475px;">')
 
-print('<h4>')
+print('<h2>')
 
 if isinstance(Coordinates, list): # if Coordinates is a list (>1 total boxes checked)
 
