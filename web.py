66a76ea1dd007d0ea8178b016915d637110329ba
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-12-11 22:27:27 +0000

    sdfg

--- web.py
+++ web.py
@@ -215,6 +215,7 @@ print("""
 .grid-container {
 display: grid;
 margin: 10px;
+float: left;
 grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
 grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
 aspect-ratio: 1;
